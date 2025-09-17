3dfba33f5d7c3ecc00432b51b0aa43f5e424a3ce
ianneyy <jimmygomez1230@gmail.com>
2025-09-17 13:19:21 +0800

    responsive header

--- src/components/ui/animated-hero.tsx
+++ src/components/ui/animated-hero.tsx
@@ -1,5 +1,28 @@
-import { PhoneCall, ArrowUpRight } from "lucide-react";
+import { PhoneCall, ArrowUpRight, MapPin } from "lucide-react";
 import { Button } from "@/components/ui/button";
+// import BlurText from "../BlurText";
+import ShinyText from "../ShinyText";
+import { AnimatedGroup } from "./animated-group";
+
+const transitionVariants = {
+  item: {
+    hidden: {
+      opacity: 0,
+      filter: "blur(12px)",
+      y: 12,
+    },
+    visible: {
+      opacity: 1,
+      filter: "blur(0px)",
+      y: 0,
+      transition: {
+        type: "spring",
+        bounce: 0.3,
+        duration: 1.5,
+      },
+    },
+  },
+};
 
 function Hero() {
   return (
@@ -12,38 +35,64 @@ function Hero() {
         alt=""
       />
       <div className="container mx-auto  relative z-99">
-        <div className="flex gap-8 py-20 lg:py-40 items-center justify-center flex-col">
-          <div>
-            <span
-              className="gap-4 py-2 px-4  rounded-full 
+        <AnimatedGroup
+          variants={{
+            container: {
+              visible: {
+                transition: {
+                  staggerChildren: 0.05,
+                  delayChildren: 0.75,
+                },
+              },
+            },
+            ...transitionVariants,
+          }}
+        >
+          <div className="flex gap-8 py-20 lg:py-40 items-center justify-center flex-col">
+            <div>
+              <div
+                className="gap-4 py-2 px-4  rounded-full 
      bg-white/10 border border-white/20 
      backdrop-blur-md  
-     text-white font-semibold text-sm"
-            >
-              Read our launch article
-            </span>
-          </div>
-          <div className="flex gap-4 flex-col">
-            <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
-              <span className="text-[#53FBA5]">Luntian Travel and Tour</span>
-            </h1>
+     text-white font-semibold text-sm flex items-center" 
+              >
+                <span>
+                  <ShinyText
+                    text="Alaminos, Pangasinan"
+                    disabled={false}
+                    speed={3}
+                    className="custom-class"
+                  />
+                </span>
+                <MapPin className="h-4 w-4 text-gray-400" />
+              </div>
+            </div>
 
-            <p className="text-lg md:text-xl leading-relaxed tracking-tight text-gray-400 max-w-2xl text-center">
-              Experience the Philippines' hidden gem through sustainable
-              ecotourism. Explore pristine natural parks, vibrant coastal
-              ecosystems, and authentic community-based adventures in Pangasinan
-              Province.
-            </p>
+            <div className="flex gap-4 flex-col">
+              {/* <span className="text-[#53FBA5]">Luntian Travel and Tour</span> */}
+
+              <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
+                <span className="text-[#53FBA5]">Luntian Travel and Tour</span>
+              </h1>
+
+              <p className="text-lg md:text-xl leading-relaxed tracking-tight text-gray-400 max-w-2xl text-center">
+                Experience the Philippines' hidden gem through sustainable
+                ecotourism. Explore pristine natural parks, vibrant coastal
+                ecosystems, and authentic community-based adventures in
+                Pangasinan Province.
+              </p>
+            </div>
+
+            <div className="flex flex-row gap-3">
+              <Button size="lg" className="gap-4" variant="outline">
+                Jump on a call <PhoneCall className="w-4 h-4" />
+              </Button>
+              <Button size="lg" className="gap-4  bg-[#53FBA5] text-black">
+                Book Now <ArrowUpRight className="w-4 h-4" />
+              </Button>
+            </div>
           </div>
-          <div className="flex flex-row gap-3">
-            <Button size="lg" className="gap-4" variant="outline">
-              Jump on a call <PhoneCall className="w-4 h-4" />
-            </Button>
-            <Button size="lg" className="gap-4  bg-[#53FBA5] text-black">
-              Book Now <ArrowUpRight className="w-4 h-4" />
-            </Button>
-          </div>
-        </div>
+        </AnimatedGroup>
       </div>
     </div>
   );
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,31 +1,98 @@
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight, Menu, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import React from "react";
+
+const menuItems = [
+  { name: "Home", href: "#home" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "/" },
+];
 
 export default function Header() {
+  const [menuState, setMenuState] = React.useState(false);
+
   return (
     <div className="py-6  w-full relative z-99">
-      <div className="flex justify-between px-16 items-center">
-        <span className="text-4xl title text-gray-100">Luntian</span>
+      <div
+        data-state={menuState && "active"}
+        className="flex  px-16 "
+      >
+        <div className="flex lg:hidden justify-between w-full">
+          <span className="text-4xl title text-gray-100">Luntian</span>
+          <button
+            onClick={() => setMenuState(!menuState)}
+            aria-label={menuState == true ? "Close Menu" : "Open Menu"}
+            data-state={menuState ? "active" : "inactive"}
+            className="group relative z-[60] -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden text-white"
+          >
+            <Menu
+              className={`m-auto size-6 duration-200 transition-all ${
+                menuState
+                  ? "rotate-180 scale-0 opacity-0"
+                  : "rotate-0 scale-100 opacity-100"
+              }`}
+            />
+            <X
+              className={`absolute inset-0 m-auto size-6 duration-200 transition-all ${
+                menuState
+                  ? "rotate-0 scale-100 opacity-100"
+                  : "-rotate-180 scale-0 opacity-0"
+              }`}
+            />
+          </button>
+        </div>
+
+        <div className="hidden lg:flex justify-between w-full">
+          <span className="text-4xl title text-gray-100">Luntian</span>
+
+          <div
+            className=" gap-5 p-2 rounded-full 
+                bg-white/10 border border-white/20 
+                backdrop-blur-md  
+                text-white font-semibold text-sm "
+          >
+            <span className="text-black bg-[#53FBA5] py-2 px-4 rounded-full cursor-pointer">
+              Home
+            </span>
+            <span className=" py-2 px-4 rounded-full cursor-pointer">
+              About
+            </span>
+            <span className=" py-2 px-4 rounded-full cursor-pointer">
+              Contact
+            </span>
+          </div>
+          <div>
+            <Button size="sm" className="gap-2 bg-[#53FBA5]">
+              <span className="text-black">Book Now</span>
+              <ArrowUpRight className="w-4 h-4 text-black" />
+            </Button>
+          </div>
+        </div>
+
         <div
-          className="flex gap-5 p-2 rounded-full 
-     bg-white/10 border border-white/20 
-     backdrop-blur-md  
-     text-white font-semibold text-sm"
+          className={`fixed inset-0 z-50  lg:hidden ${
+            menuState ? "block" : "hidden"
+          }`}
         >
-          <span className="text-black bg-[#53FBA5] py-2 px-4 rounded-full cursor-pointer">
-            Home
-          </span>
-          <span className=" py-2 px-4 rounded-full cursor-pointer">About</span>
-          <span className=" py-2 px-4 rounded-full cursor-pointer">
-            Contact
-          </span>
+          <div className="absolute top-20 right-4 bg-white/10 backdrop-blur-md border border-white/20 rounded-3xl p-6 min-w-[200px]">
+            <ul className="space-y-6 text-base">
+              {menuItems.map((item, index) => (
+                <li key={index}>
+                  <a
+                    href={item.href}
+                    className="text-white hover:text-[#53FBA5] block duration-150 text-lg font-medium"
+                    onClick={() => setMenuState(false)}
+                  >
+                    <span>{item.name}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div>
-          <Button size="sm" className="gap-2 bg-[#53FBA5]">
-            <span className="text-black">Book Now</span>
-            <ArrowUpRight className="w-4 h-4 text-black" />
-          </Button>
-        </div>
+        {/* <Menu className="h-6 w-6 block lg:hidden text-white" /> */}
       </div>
     </div>
   );
